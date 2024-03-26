393ed40fe52b078ac7d11f5aed643355c36729e9
SpencerRaw <54766197+SpencerRaw@users.noreply.github.com>
2024-03-26 15:46:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 
 with st.sidebar:
-    st.title('🤖💬 OpenAI Chatbot')
+    st.title('🤖💬 SLE Doctor Chatbot')
     if 'OPENAI_API_KEY' in st.secrets:
         st.success('API key already provided!', icon='✅')
         # openai.api_key = st.secrets['OPENAI_API_KEY']
